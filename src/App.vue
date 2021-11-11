35b8d4fa5dd8e836f58f70bb08e3581c8cc11fbe
Andretreccia <andreabtreccia@gmail.com>
2021-11-11 15:30:53 +0100

    Commit iniziale
    Creazione dei file necessari e della loro struttura

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import x from "./components/x.vue";
+
+export default {
+  name: "App",
+  components: {
+   
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
