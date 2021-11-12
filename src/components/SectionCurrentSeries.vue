5778710f008d0b43190c733c01ddde74c3c5ed71
Andretreccia <andreabtreccia@gmail.com>
2021-11-12 11:34:07 +0100

    Creato un nuovo componente da inserire nel main

---
+++ src/components/SectionCurrentSeries.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>pappa</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,8 +1,11 @@
 <template>
     <main id="site_main">
+      <div class="jumbotron">
+        <img src="../assets/img/jumbotron.jpg" alt="">
+      </div>
       <section> 
         <div class="container">
-          <h1> -->CONTENT GOES HERE  </h1>
+         <SectionCurrentSeries/>
         </div>
       </section>
      
@@ -13,15 +16,25 @@
 
 <script>
 import ShopSection from "./ShopSection.vue";
+import SectionCurrentSeries from "./SectionCurrentSeries.vue";
 export default{
   components:{
     ShopSection,
+    SectionCurrentSeries,
   }
 }
 </script>
 
 <style lang="scss">
 #site_main{
+  .jumbotron{
+    height: 400px;
+    width: 100%;
+    overflow: hidden;
+      img{
+        width: 100%;
+      }
+  }
   section{
     height: 120px;
     background-color: #1C1C1C;
